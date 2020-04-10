a73f9869a6232068123ca92b755b28b2415d5c40
rachitrawat <rachit.rawatr@gmail.com>
2020-04-10 14:04:52 +0530

    update sciprts

--- fabchat/testFabChat.sh
+++ fabchat/testFabChat.sh
@@ -6,11 +6,15 @@ node enrollAdmin.js
 node registerUser.js user1
 node registerUser.js user2
 node registerUser.js user3
+node invoke.js createMsg '$HELLO$' user1 u1@ashoka.edu.in
+node invoke.js createMsg '$HELLO$' user2 u2@ashoka.edu.in
+node invoke.js createMsg '$HELLO$' user3 u3@ashoka.edu.in
 node invoke.js createMsg hello user1 u1@ashoka.edu.in
 node invoke.js createMsg welcome user2 u2@ashoka.edu.in
 node invoke.js createMsg covid19 user3 u3@ashoka.edu.in
 node query.js -1 user1
-node query.js 2 user1
-node invoke.js flagMsg 2 user1
-node invoke.js flagMsg 2 user2
-node query.js 2 user1
+node query.js 3 user1
+node invoke.js flagMsg 3 user1
+node query.js 3 user1
+node invoke.js flagMsg 3 user2
+node query.js 3 user1
